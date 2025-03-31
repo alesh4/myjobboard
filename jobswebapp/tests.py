8162f44f90bb9297566c3b7bf36b09f291312098
alex <alexh_714@yahoo.com>
2025-03-30 21:55:57 -0700

    added simple test AND changed github action for simple tests

--- jobswebapp/tests.py
+++ jobswebapp/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+#from jobswebapp.models import Job
 
-# Create your tests here.
+class MyJobBoardTests(TestCase):
+    def setUp(self):
+        #job = Job()
+        pass
+
+    def test_main_list_url_returns_success(self):
+        response = self.client.get('/jobs/')
+        self.assertEqual(response.status_code,200)
+        
