b063c846d4be59b70ba6cd9e83bee013edfe6824
alex <alexh_714@yahoo.com>
2025-04-03 11:32:20 -0700

    changed the timeframe for jobs to be last 2 days again

--- jobswebapp/views.py
+++ jobswebapp/views.py
@@ -6,11 +6,9 @@ from .models import Job
 
 # Create your views here.
 def index(request):
-    from_date = date.today() - timedelta(days=8) #change delta accordandly
+    from_date = date.today() - timedelta(days=2) #change delta accordandly
     job_list = Job.objects.filter(date_posted__gte=from_date, location__contains="Seattle")
-    #print(job_list[0])
     json_list = json.dumps(list(job_list.values()), default=str)
-    #print(json_list)
     return render(request, "jobswebapp/index.html", {"jobs":job_list, "json_data":json_list})
 
 def jobsinlocation(request, location):
