4f6194954774a93d17ce17555ce362826b3406fc
alex <alexh_714@yahoo.com>
2025-04-03 14:45:36 -0700

    type fix

--- core/settings.py
+++ core/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'DJANGO-SECRET_KEY','django-insecure-je0s2q5a9dz!*uy$#2=!tr^gmj%be6t^g^*^ys^bn*p#e&(745'
+SECRET_KEY = 'django-insecure-je0s2q5a9dz!*uy$#2=!tr^gmj%be6t^g^*^ys^bn*p#e&(745'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
