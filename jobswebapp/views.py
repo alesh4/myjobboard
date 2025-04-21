88f987d38d190c2ae08f6847c098121d7ac26004
alex <alexh_714@yahoo.com>
2025-04-20 19:48:54 -0700

    added flag for latest only jobs

--- jobswebapp/views.py
+++ jobswebapp/views.py
@@ -12,14 +12,19 @@ def index(request):
     return render(request, "jobswebapp/index.html", {"jobs":job_list, "json_data":json_list})
 
 def jobsinlocation(request, location):
-    location_converter = {'fresno':'Fresno, CA','bakersfield':'Bakersfield, CA', 'seattle':'Seattle, WA'}
-    if location not in ['fresno','bakersfield','seattle']:
+    location_converter = {'fresno':'Fresno, CA','bakersfield':'Bakersfield, CA', 'seattle':'Seattle, WA', 'houston':'Houston, TX'}
+    if location not in ['fresno','bakersfield','seattle','houston']:
         return redirect("/jobs/")
     else:
         location = location_converter[location]
 
-    from_date = date.today() - timedelta(days=8) #change delta accordandly
-    job_list = Job.objects.filter(date_posted__gte=from_date, location=location)
+    latest_only = request.GET.get('lo',None)
+    from_date = date.today() - timedelta(days=2) #change delta accordandly
+    job_list = Job.objects.filter(location=location)
+    latest_only = False if latest_only in ['False','f','false','0'] else True
+    if latest_only:
+        job_list = job_list | Job.objects.filter(date_posted__gte=from_date)
+
     json_list = json.dumps(list(job_list.values()), default=str)
     return render(request, "jobswebapp/index.html", {"jobs":job_list, "json_data":json_list})
 
