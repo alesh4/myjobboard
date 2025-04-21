a3bf699fa769e351e40c23814de8ec5ce557a739
alex <alexh_714@yahoo.com>
2025-04-21 14:07:58 -0700

    fixed my ORM query and changed location to LIKE instead of exact

--- jobswebapp/views.py
+++ jobswebapp/views.py
@@ -15,15 +15,13 @@ def jobsinlocation(request, location):
     location_converter = {'fresno':'Fresno, CA','bakersfield':'Bakersfield, CA', 'seattle':'Seattle, WA', 'houston':'Houston, TX'}
     if location not in ['fresno','bakersfield','seattle','houston']:
         return redirect("/jobs/")
-    else:
-        location = location_converter[location]
 
     latest_only = request.GET.get('lo',None)
     from_date = date.today() - timedelta(days=2) #change delta accordandly
-    job_list = Job.objects.filter(location=location)
+    job_list = Job.objects.filter(location_contains=location)
     latest_only = False if latest_only in ['False','f','false','0'] else True
     if latest_only:
-        job_list = job_list | Job.objects.filter(date_posted__gte=from_date)
+        job_list = job_list & Job.objects.filter(date_posted__gte=from_date)
 
     json_list = json.dumps(list(job_list.values()), default=str)
     return render(request, "jobswebapp/index.html", {"jobs":job_list, "json_data":json_list})
